2f95801ab5acc9c1b89085a6071caa1b2bb73dd6
ssaammyy <55077107+SaemsolYoo@users.noreply.github.com>
2020-08-16 22:49:57 +0900

    update greetings.js

--- bg.js
+++ bg.js
@@ -2,15 +2,9 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-/* 이미지를 API에서 가져오는거였다면 필요햇을 코드
-
-function handleImgLoad(){
-
+function handleImgLoad() {
   console.log("finished loading");
-
 }
-
-*/
 
 function paintImage(imgNumber) {
   const image = new Image();
@@ -21,11 +15,7 @@ function paintImage(imgNumber) {
 
   body.appendChild(image);
 
-  /* 이미지를 API에서 가져오는거였다면 필요햇을 코드
-
-  image.addEventListener("loadend",handleImgLoad);
-
-  */
+  image.addEventListener("loadend", handleImgLoad);
 }
 
 function genRandom() {
@@ -33,12 +23,6 @@ function genRandom() {
 
   return number;
 }
-
-// Math.floor 는 숫자를 버림 해주는것. 3.4--> 3으로
-
-// Math.random() * n 는 0부터 5까지 숫자 랜덤으로 잔뜩 나오는거 2.324346 막 이런식으루,,
-
-// 그래서 소수점 많이나온는거 먼저 앞자리 해서 해주고, 그담에 버림 해주는 함수 써준거다.
 
 function init() {
   const randomNumber = genRandom();
